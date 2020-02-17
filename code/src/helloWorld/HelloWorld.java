c943d909243c103ffd0092dc8d5b266684ceb041
merroneutron <2501451141@qq.com>
2020-02-17 15:52:45 +0800

    .

--- code/src/helloWorld/HelloWorld.java
+++ code/src/helloWorld/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO 自动生成的方法存根
 		System.out.println(a+"你好，java——git");
+		System.out.println("Hello,eclipse git!");
 	}
 	
 
