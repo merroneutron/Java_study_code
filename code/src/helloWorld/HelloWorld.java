6806b8019d290b071aa0d6f2915f89a2b106402d
merroneutron <2501451141@qq.com>
2020-02-17 22:45:21 +0800

    add StreamInTest.java

--- code/src/helloWorld/HelloWorld.java
+++ code/src/helloWorld/HelloWorld.java
@@ -1,7 +1,8 @@
 package helloWorld;
 
 public class HelloWorld {
-	static int a = 0;
+	static int b=1,c=2;
+	static int a=b+c/10;//b,c已经定义
 	public static void main(String[] args) {
 		// TODO 自动生成的方法存根
 		System.out.println(a+"你好，java——git");
