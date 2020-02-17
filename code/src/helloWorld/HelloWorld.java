acfef3945a22d985f4a17060817a057a19608663
merroneutron <2501451141@qq.com>
2020-02-17 15:50:20 +0800

    .

--- code/src/helloWorld/HelloWorld.java
+++ code/src/helloWorld/HelloWorld.java
@@ -1,10 +1,11 @@
 package helloWorld;
 
 public class HelloWorld {
-
+	static int a = 0;
 	public static void main(String[] args) {
 		// TODO 自动生成的方法存根
-		System.out.println("你好，java——git");
+		System.out.println(a+"你好，java——git");
 	}
+	
 
 }
